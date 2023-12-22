b1f78c80938a75bf39c42a629f9b1e35467799ca
Kenshi2727 <abhishekmathurofficial@gmail.com>
2023-12-22 18:21:12 +0530

    added variables.js and kenshi.js

---
+++ kenshi.js
@@ -0,0 +1,4 @@
+console.log("kenshi.js loaded!!!!!yeah!!")
+console.log("Welcome to the world of full stack web development!")
+console.log("I am damn sure to integrate it with data science and AI ML fields")
+console.log("earlier code was not running because I'd not saved the .js file")
---
+++ variables.js
@@ -0,0 +1,63 @@
+// let name1 = 'abhishek'
+// let name2 = 'mathur'
+// let name3 = 'kenshi'
+// console.log(name1, name2, name3)
+// console.log(typeof(name1))
+
+// let age=21.78
+// console.log(age)
+// console.log(typeof(age))
+
+// const condition1 = true
+// const condition2 = false
+// console.log(typeof(condition1))
+
+// let values=NaN
+// console.log(typeof(values))
+
+// let value=null
+// console.log(typeof(value))
+
+// let value2=undefined
+// console.log(typeof(value2))
+
+// let value3=Symbol('hello')
+// console.log(typeof(value3))
+// console.log(value3)
+
+// let bigvalue=BigInt(1234567890123456789012345678901234567890)
+// let bigvalue2=1234567890123456789012345678901234567890n
+// console.log(typeof(bigvalue))
+// console.log(typeof(bigvalue2))
+
+// let age=21
+// let name='abhishek'
+// console.log("my name is "+name+" and my age is "+age)
+// console.log(`my name is ${name} and my age is ${age}`)
+
+// let studentDetails={
+//     name:'abhishek',
+//     age:21,
+//     marks: [10,20,30,40,50],
+//     address:{
+//         city:'jaipur',
+//         state:'rajasthan'
+//     }
+// }
+
+// console.log(studentDetails)
+// console.log(studentDetails.marks[2])
+let num1=3
+let num2='3'
+
+// console.log(`the sum of ${num1} and ${num2} is ${num1+num2}`)
+// console.log(`the sub of ${num1} and ${num2} is ${num1-num2}`)
+// console.log(`the mul of ${num1} and ${num2} is ${num1*num2}`)
+// console.log(`the div of ${num1} and ${num2} is ${num1/num2}`)
+// console.log(`the remainder of ${num1} and ${num2} is ${num1%num2}`)
+// console.log(`the power of ${num1} and ${num2} is ${num1**num2}`)
+// console.log(`the increment of ${num1} is ${num1++}`)
+// console.log(`the decrement of ${num1} is ${num1--}`)
+
+console.log(num1!=num2)
+console.log(num1!==num2)
